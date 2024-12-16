e822c377ef9a868ad59fea6b1b984deb59d32555
genGit963 <bogati.mahesh.299.792.458@gmail.com>
2024-12-16 12:48:37 +0545

    ✅: Reporting MUTEX: solution fixed

--- mutex/normal_mutex.go
+++ mutex/normal_mutex.go
@@ -2,31 +2,47 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
-var sharedResources int = 100
+var (
+	sharedResources int = 100
+	lock                = sync.Mutex{}
+)
 
 // add to sharedResources
 func stingy() {
-	sharedResources = sharedResources + 10
-	time.Sleep(1 * time.Millisecond)
+	// executing 1000 different threads of stingy
+	lock.Lock()
+	for i := 0; i < 1000; i++ {
+		sharedResources = sharedResources + 10
+		time.Sleep(1 * time.Millisecond)
 
+	}
+	lock.Unlock()
 	fmt.Println("done stingy !!")
 }
 
 // remove from sharedResources
 func spendy() {
-	sharedResources = sharedResources + 10
-	time.Sleep(1 * time.Millisecond)
+	// executing 1000 different threads of spendy
+	lock.Lock()
+	for i := 0; i < 1000; i++ {
+		sharedResources = sharedResources - 10
+		time.Sleep(1 * time.Millisecond)
+	}
+	lock.Unlock()
 	fmt.Println("done spendy !!")
 }
 
 func main() {
-	// executing 100 different threads of stingy and spendy
-	for i := 0; i < 100; i++ {
-		go stingy()
-		go spendy()
-	}
-	fmt.Println("done !")
+
+	go stingy()
+	go spendy()
+
+	// sleep for 5 second
+	time.Sleep(3 * time.Second)
+	// then result
+	fmt.Println(sharedResources, "done !")
 }
