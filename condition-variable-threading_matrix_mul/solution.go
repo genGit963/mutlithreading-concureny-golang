324758f6e2f876c4f356df6aed57eda3e8bb1bf6
genGit963 <bogati.mahesh.299.792.458@gmail.com>
2024-12-20 23:45:13 +0545

    SOLUTION:- condition variable

--- condition-variable-threading_matrix_mul/solution.go
+++ condition-variable-threading_matrix_mul/solution.go
@@ -6,26 +6,42 @@ import (
 	"time"
 )
 
+// CV: condition variable
+
 var (
-	money = 100
-	lock  = sync.Mutex{}
+	moneyBank = 100
+	mutexLock = sync.Mutex{}
+
+	// condition variable sync
+	moneyDeposited = sync.NewCond(&mutexLock)
 )
 
-func stingy() {
+func stingyWithCV() {
 	for i := 0; i < 1000; i++ {
 		lock.Lock()
-		money += 10
+		moneyBank += 10
 		fmt.Println("stingy updated to : ", money)
+
+		// broadcast
+		moneyDeposited.Broadcast()
+
 		lock.Unlock()
 		time.Sleep(1 * time.Millisecond)
 	}
 	fmt.Println("Stingy Done")
 }
 
-func spendy() {
+func spendyWithCV() {
 	for i := 0; i < 1000; i++ {
+
+		// 1. mutex lock
 		lock.Lock()
-		money -= 20
+		// 2. wait-broadcast-signal
+		for moneyBank-20 < 0 {
+			// wait for 20$ in money
+			moneyDeposited.Wait()
+		}
+		moneyBank -= 20
 		fmt.Println("spendy reduced to : ", money)
 		lock.Unlock()
 		time.Sleep(1 * time.Millisecond)
@@ -33,10 +49,10 @@ func spendy() {
 	fmt.Println("spendy Done")
 }
 
-func MainIssue() {
+func main() {
 
-	go stingy()
-	go spendy()
+	go stingyWithCV()
+	go spendyWithCV()
 
 	time.Sleep(3 * time.Second)
 	fmt.Println("Last: ", money)
